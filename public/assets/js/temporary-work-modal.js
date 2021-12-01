ae07634a118330a3871baa5015004cac75c8a9b8
obaidjani <muhammad.obaid@dazaran.com>
2021-12-01 13:07:58 +0500

    list selectiong and show according to data

--- public/assets/js/temporary-work-modal.js
+++ public/assets/js/temporary-work-modal.js
@@ -1,76 +1,101 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var val, date, otherVal;
-    $(".modalDiv input").on("click",function(){
-        $(this).addClass("active")
-        val=$(this);
-        $(".submit-requirment button").attr("disabled","disabled")
-        $(".submit-requirment button").css("opacity",".5")
-    })
-    $("#design-requirement .requirment-first ul li").click(function(){
-        $(".requirment-first ul li").removeClass("active")
-        $(this).addClass("active")
-        $(".requirment-second").css("display","block");
-        var val=$(this).text();
+    $(".modalDiv input").on("click", function () {
+        $(this).addClass("active");
+        val = $(this);
+        $(".submit-requirment button").attr("disabled", "disabled");
+        $(".submit-requirment button").css("opacity", ".5");
+    });
+    $("#design-requirement .requirment-first ul li").click(function () {
+        $(".requirment-first ul li").removeClass("active");
+        $(this).addClass("active");
+        $(".requirment-second").css("display", "block");
+        var val = $(this).text();
         $(".requirment-first-value").val(val);
-
-    })
-    $("#design-requirement .requirment-second ul li").click(function(){
-            $(".requirment-second ul li").removeClass("active")
-            $("#design-requirement .requirment-second ul li input").removeClass("active");
-            $(this).addClass("active")
-            var val=$(this).text();
-            $(".requirment-second-value").val(val);
-            $("#design-requirement .requirment-second ul li.active input").addClass("active");
-            $(".submit-requirment button").removeAttr("disabled")
-            $(".submit-requirment button").css("opacity","1")
-    })
-    $('.otherInput').on('input',function(e){
-        otherVal=$(this).val();
+    });
+    $("#design-requirement .requirment-second ul li").click(function () {
+        $(".requirment-second ul li").removeClass("active");
+        $("#design-requirement .requirment-second ul li input").removeClass(
+            "active"
+        );
+        $(this).addClass("active");
+        var val = $(this).text();
+        $(".requirment-second-value").val(val);
+        $("#design-requirement .requirment-second ul li.active input").addClass(
+            "active"
+        );
+        $(".submit-requirment button").removeAttr("disabled");
+        $(".submit-requirment button").css("opacity", "1");
+    });
+    $(".otherInput").on("input", function (e) {
+        otherVal = $(this).val();
         $(".requirment-second-value").val(otherVal);
     });
-    $("#design-requirement .submit-requirment button").click(function(){
-        var val_first= $(".requirment-first-value").val();
-        var val_second= $(".requirment-second-value").val();
-        var full_val= val_first+' - '+val_second;
-        val.attr("value",full_val);
-    })
+    $("#design-requirement .submit-requirment button").click(function () {
+        var val_first = $(".requirment-first-value").val();
+        var val_second = $(".requirment-second-value").val();
+        var full_val = val_first + " - " + val_second;
+        val.attr("value", full_val);
+    });
 
-    $("#scope-of-design .requirment-first ul li").click(function(){
-        $("#scope-of-design .requirment-first ul li").removeClass("active")
-        $(this).addClass("active")
-        $("#scope-of-design .requirment-second").css("display","block");
-        var val=$(this).text();
+    $("#scope-of-design .requirment-first ul li").click(function () {
+        $("#scope-of-design .requirment-first ul li").removeClass("active");
+        $(this).addClass("active");
+        id = $(this).attr("data-id");
+        $("#scope-of-design .requirment-second").css("display", "block");
+        $("li.d-none." + id + "")
+            .removeClass("d-none")
+            .css("display", "block");
+
+        var val = $(this).text();
         $("#scope-of-design .requirment-first-value").val(val);
-    })
-    $("#scope-of-design .requirment-second ul li").click(function(){
-        $("#scope-of-design .requirment-second ul li").removeClass("active")
-        $("#scope-of-design .requirment-second ul li input").removeClass("active")
-        $(this).addClass("active")
-        $("#scope-of-design .requirment-second ul li.active input").addClass("active");
-        $("#scope-of-design .requirment-second").css("display","block");
-        $("#scope-of-design .submit-requirment button").removeAttr("disabled")
-        $("#scope-of-design .submit-requirment button").css("opacity","1")
-    })
-    $("#scope-of-design .submit-requirment button").click(function(){
-        var val_first= $("#scope-of-design .requirment-first-value").val();
-        var val_second= $("#scope-of-design .requirment-second-value").val();
-        var full_val= val_first+' - '+val_second;
-        console.log($("#scope-of-design .requirment-first-value").val())
+    });
+    $("#scope-of-design .requirment-second ul li").click(function () {
+        $("#scope-of-design .requirment-second ul li").removeClass("active");
+        $("#scope-of-design .requirment-second ul li input").removeClass(
+            "active"
+        );
+        $(this).addClass("active");
+        $("#scope-of-design .requirment-second ul li.active input").addClass(
+            "active"
+        );
+        $("#scope-of-design .requirment-second").css("display", "block");
+        $("#scope-of-design .submit-requirment button").removeAttr("disabled");
+        $("#scope-of-design .submit-requirment button").css("opacity", "1");
+    });
+    $("#scope-of-design .submit-requirment button").click(function () {
+        var val_first = $("#scope-of-design .requirment-first-value").val();
+        var val_second = $("#scope-of-design .requirment-second-value").val();
+        var full_val = val_first + " - " + val_second;
+        console.log($("#scope-of-design .requirment-first-value").val());
         val.val(full_val);
         $(".requirment-first-value").val(null);
         $(".requirment-second-value").val(null);
-    })
-    $("#scope-of-design .requirment-second ul li input").change(function(){
-        date=new Date($(this).val());
-        date=date.getFullYear() + "/" + (date.getMonth()+1) + "/" + date.getDate();
+    });
+    $("#scope-of-design .requirment-second ul li input").change(function () {
+        date = new Date($(this).val());
+        date =
+            date.getFullYear() +
+            "/" +
+            (date.getMonth() + 1) +
+            "/" +
+            date.getDate();
         $("#scope-of-design .requirment-second-value").val(date);
-    })
-    $("#attachment-of-design .requirment-first ul li").click(function(){
-        $("#attachment-of-design .submit-requirment button").removeAttr("disabled")
-        $("#attachment-of-design .requirment-first ul li").removeClass("active")
-        $(this).addClass("active")
-        $("#attachment-of-design .requirment-second").css("display","block");
-        var val=$(this).text();
+    });
+    $("#attachment-of-design .requirment-first ul li").click(function () {
+        $("#attachment-of-design .submit-requirment button").removeAttr(
+            "disabled"
+        );
+        $("#attachment-of-design .requirment-first ul li").removeClass(
+            "active"
+        );
+        $(this).addClass("active");
+        id = $(this).attr("data-id");
+        $("#attachment-of-design .requirment-second").css("display", "block");
+        $("li.d-none." + id + "")
+            .removeClass("d-none")
+            .css("display", "block");
+        var val = $(this).text();
         $("#attachment-of-design .requirment-first-value").val(val);
-    })
-})
+    });
+});

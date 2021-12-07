2b05a8436c7d518f483a6322da92da2ded0c5dbc
obaidjani <muhammad.obaid@dazaran.com>
2021-12-07 17:38:05 +0500

    some changes done

--- resources/views/mail/temporaryworkmail.blade.php
+++ resources/views/mail/temporaryworkmail.blade.php
@@ -1,18 +1,63 @@
-<div class="background-color:#454545;">Hello</div><br>
-
-Welcome to Contstruction Site, an online Project platform designed to help you become the best version of yourself, alongside people who share your passions.You are all set to begin.
-
-You are receiving this email because you have involve this project.
-<br>
-<div style="text-align:center;"><a style="margin-right:10px;color:#F26C20;" href="https://tobyo18.sg-host.com/blogs/">Blog</a><a style="color:#F26C20;" href="https://tobyo18.sg-host.com/contact/">Contact
-        us</a></div>
-<div style="margin-top:20px;text-align:center;">
-    <a href="https://www.facebook.com/evateachlms/"><img style="width:40px;height:40px;" src="https://tobyo18.sg-host.com/wp-content/uploads/2021/03/fb.png" /></a>
-    <a href="https://www.linkedin.com/company/evateach/about/"><img style="width:40px;height:40px;" src="https://tobyo18.sg-host.com/wp-content/uploads/2021/03/linkedin.png" /></a>
-    <a href="https://twitter.com/evateach2"><img style="width:40px;height:40px;" src="https://tobyo18.sg-host.com/wp-content/uploads/2021/03/twitter.png" /></a>
-    <a href="https://www.instagram.com/_evateach/"><img style="width:40px;height:40px;" src="https://tobyo18.sg-host.com/wp-content/uploads/2021/03/instagram.png" /></a>
-</div>
-<div style="margin-top:20px;text-align:center;">
-    <p style="text-align:center;color:#777;">Suite 2A, Blackthorn House, St Pauls Square,</p>
-    <p style="text-align:center;color:#777;">Birmingham. B3 1RL, United Kingdom</p>
+<div class="">
+    <div id=":p0" tabindex="-1"></div>
+    <div id=":op" class="ii gt" jslog="20277; u014N:xr6bB; 4:W251bGwsbnVsbCxbXV0.">
+        <div id=":oo" class="a3s aiL "><u></u>
+            <div style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;background-color:#f5f8fa;color:#74787e;height:100%;line-height:1.4;margin:0;width:100%!important;word-break:break-word">
+                <table width="100%" cellpadding="0" cellspacing="0" style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;background-color:#f5f8fa;margin:0;padding:0;width:100%">
+                    <tbody>
+                        <tr>
+                            <td align="center" style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box">
+                                <table width="100%" cellpadding="0" cellspacing="0" style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;margin:0;padding:0;width:100%">
+                                    <tbody>
+                                        <tr>
+                                            <td style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;padding:25px 0;text-align:center;color:white;background:#131313">
+                                                <a href="http://construction.accrualhub.com/" style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#bbbfc3;font-size:19px;font-weight:bold;text-decoration:none" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://demo.moray-limousines.com/&amp;source=gmail&amp;ust=1638963291925000&amp;usg=AOvVaw1hllcMnVjrBhGsusEn01lQ">
+                                                    Construction Management
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td width="100%" cellpadding="0" cellspacing="0" style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;background-color:#ffffff;border-bottom:1px solid #edeff2;border-top:1px solid #edeff2;margin:0;padding:0;width:100%">
+                                                <table align="center" width="570" cellpadding="0" cellspacing="0" style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;background-color:#ffffff;margin:0 auto;padding:0;width:570px">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;padding:35px"><span class="im">
+                                                                    <h1 style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#2f3133;font-size:19px;font-weight:bold;margin-top:0;text-align:left">Hello!</h1>
+                                                                    <p style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left">Welcome to the online Temporary Works Portal. Attached is a PDF design created for (company, site and address are as stated in the design brief). Relevant attachments for the design brief are also included.</p>
+                                                                    @if(count($details['body']['links'])>0)
+                                                                    <h3>Uploaded Images</h3>
+                                                                    @foreach($details['body']['links'] as $link)
+                                                                    <a href="{{asset($link)}}">{{$link}}</a>
+                                                                    @endforeach
+                                                                    @endif
+                                                                    <p style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left">Regards,<br>Construction Management</p>
+                                                                </span>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box">
+                                                <table align="center" width="570" cellpadding="0" cellspacing="0" style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;margin:0 auto;padding:0;text-align:center;width:570px">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td align="center" style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;padding:35px">
+                                                                <p style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;line-height:1.5em;margin-top:0;color:#aeaeae;font-size:12px;text-align:center">© 2021 Contstruction Management. All rights reserved.</p>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>

b608ae8b1bb73a56a66ad6b258d439f358617388
Abdul Basit <basitawan.abdul@gmail.com>
2022-01-13 13:30:51 +0500

    Update TemporaryWorkController.php

--- app/Http/Controllers/TemporaryWorkController.php
+++ app/Http/Controllers/TemporaryWorkController.php
@@ -534,7 +534,7 @@ class TemporaryWorkController extends Controller
                     $status = "Unloaded";
                 }
                  $path =config('app.url');
-                $list .= '<tr style="' . $class . '"><td><a target="_blank" href="'.$path .' pdf/' . $permit->ped_url . '">Pdf Link</a></td><td>'.$permit->permit_no.'</td><td>' . $permit->created_at->diffForHumans() . '</td><td>Permit Load</td><td>' .  $status . '</td><td>' . $button . '</td></tr>';
+                $list .= '<tr style="' . $class . '"><td><a target="_blank" href="'.$path .'pdf/' . $permit->ped_url . '">Pdf Link</a></td><td>'.$permit->permit_no.'</td><td>' . $permit->created_at->diffForHumans() . '</td><td>Permit Load</td><td>' .  $status . '</td><td>' . $button . '</td></tr>';
             }
             $list .= '<hr>';
         }

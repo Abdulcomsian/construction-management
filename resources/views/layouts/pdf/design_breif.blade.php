102d390fc25566587aea5b97a3698f96a54673f5
Abdul Basit <basitawan.abdul@gmail.com>
2022-01-13 13:47:18 +0500

    Update design_breif.blade.php

--- resources/views/layouts/pdf/design_breif.blade.php
+++ resources/views/layouts/pdf/design_breif.blade.php
@@ -395,6 +395,7 @@
                             {{ucwords($data['namesign'])}}
                             @else
                             @php $sign=\App\Models\TemporaryWork::find($image_name);@endphp
+                            {{asset('/public/temporary/signature') .'/'. $sign->signature}}
                             <img src="{{asset('/public/temporary/signature') .'/'. $sign->signature}}" width="auto" height="120">
                             @endif
                         </td>

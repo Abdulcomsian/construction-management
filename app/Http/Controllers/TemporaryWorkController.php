c5c5c09eaa07cd0dcff6dcf271fc61444ba2ad74
Abdul Basit <basitawan.abdul@gmail.com>
2022-01-14 13:00:31 +0500

    Update TemporaryWorkController.php

--- app/Http/Controllers/TemporaryWorkController.php
+++ app/Http/Controllers/TemporaryWorkController.php
@@ -866,7 +866,7 @@ class TemporaryWorkController extends Controller
     {
         try {
             $data = TemporaryWork::with('temp_work_images', 'uploadfile', 'permits', 'scaffold')->find($id);
-            Notification::route('mail', 'hani.thaher@gmail.com')->notify(new TempAttachmentNotifications($data));
+            Notification::route('mail', 'basitawan.abdul@gmail.com')->notify(new TempAttachmentNotifications($data));
             toastSuccess('Attachements sent successfully!');
             return Redirect::back();
         } catch (\Exception $exception) {

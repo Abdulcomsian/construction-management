2e6e82994cf8fef531ae8fdd20ebacd3adf4df8a
Abdul Basit <basitawan.abdul@gmail.com>
2022-01-27 14:16:26 +0500

    updated code

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/temporary_works';
 
     /**
      * The controller namespace for the application.

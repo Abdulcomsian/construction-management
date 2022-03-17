972582a609b744d49392815311b9c11bc3912a26
obaidjani <muhammad.obaid@dazaran.com>
2022-03-17 15:50:20 +0500

    changes done

--- app/Http/Controllers/DesignerController.php
+++ app/Http/Controllers/DesignerController.php
@@ -33,10 +33,16 @@ class DesignerController extends Controller
             if(isset($request->designcheckfile))
             {
                 $file = $request->file('designcheckfile');
+                $ext= $request->file('designcheckfile')->extension();
+                $subject='Designer Uploaded Certificate';
+                $text=' Welcome to the online i-works Web-Portal.Designer have uploaded Design Check Certificate. Please Login and view document.';
                 $file_type=2;
             }
             else{
                     $file = $request->file('file');
+                    $ext= $request->file('file')->extension();
+                    $subject='Designer Uploaded Drawing';
+                     $text=' Welcome to the online i-works Web-Portal. Designer have uploaded Drawing. Please Login and view drawing.';
                     $model->drawing_number=$request->drawing_number;
                     $model->comments=$request->comments;
                     $model->twd_name=$request->twd_name;
@@ -44,6 +50,7 @@ class DesignerController extends Controller
                     $model->preliminary_approval=$request->preliminary_approval;
                     $model->construction=$request->construction;
             }
+
             $imagename = HelperFunctions::saveFile(null, $file, $filePath);
             $model->file_name = $imagename;
             $model->file_type = $file_type;
@@ -53,12 +60,13 @@ class DesignerController extends Controller
                
                   $notify_admins_msg = [
                     'greeting' => 'Designer Upload Document',
-                    'subject' => 'Designer Uploaded Document',
+                    'subject' => $subject,
                     'body' => [
-                        'text' => '',
-                        'filename' =>$imagename,
+                        'text' => $text,
+                        'filename' =>$tempworkdata->ped_url,
                         'links' =>  '',
-                        'name' => 'Design Upload',
+                        'name' => $request->drawing_title.'-'.$request->drawing_number,
+                        'ext'=> $ext,
                     ],
                     'thanks_text' => 'Thanks For Using our site',
                     'action_text' => '',
@@ -86,6 +94,7 @@ class DesignerController extends Controller
          $list.='<th>TWD Name</th><th>Drawing Title</th><th>Preliminary/ For approval</th><th>For construction</th><th>Action</th>';
          $list.='</tr></thead><tbody>';
          $i=1;
+          $path = config('app.url');
          foreach($DesignerUploads as $uploads)
          {
              $papproval='No';
@@ -100,7 +109,7 @@ class DesignerController extends Controller
                 $construction='Yes';
              }
 
-            $list.='<tr style="background:'.$background.'">';
+            $list.='<tr class="clickable-row cursor-pointer" data-href="' . $path . '/'.$uploads->file_name.'" style="background:'.$background.'">';
             $list.='<td>'.$i.'</td>';
             $list.='<td>'.$uploads->drawing_number.'</td>';
             $list.='<td>'.$uploads->comments.'</td>';
@@ -108,16 +117,15 @@ class DesignerController extends Controller
             $list.='<td>'.$uploads->drawing_title.'</td>';
             $list.='<td>'. $papproval.'</td>';
             $list.='<td>'.$construction.'</td>';
-            $path = config('app.url');
             if($construction=='Yes')
             {
             $list.='<td style="display:flex">
                      <a href="' . $path . '/'.$uploads->file_name.'" target="_blank">D'.$i.'</a>&nbsp;
-                     <form method="get" action="'. route("permit.load").'" style="display:inline-block;">
+                     <form id="submit'.$uploads->id.'" method="get" action="'. route("permit.load").'" style="display:inline-block;">
                         <input type="hidden" class="temp_work_id" name="temp_work_id" value='.Crypt::encrypt($tempworkid).' />
                         <input type="hidden"  name="drawingno" value='.$uploads->drawing_number.' />
                          <input type="hidden"  name="drawingtitle" value='.$uploads->drawing_title.' />
-                        <button style="font-size:8px" type="submit" class="btn btn-primary btn-small" id="permiturl">Open Permit</button>
+                        <button style="font-size:8px" type="button" class="btn btn-primary btn-small openpermitform" id="'.$uploads->id.'">Open Permit</button>
                     </form>
                     </td>';
                 }

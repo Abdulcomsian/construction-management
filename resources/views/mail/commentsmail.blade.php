329a47a14df6e2cfbd128d65e5d08860756c1274
obaidjani <muhammad.obaid@dazaran.com>
2022-04-05 12:32:50 +0500

    contruction changes done

--- resources/views/mail/commentsmail.blade.php
+++ resources/views/mail/commentsmail.blade.php
@@ -25,7 +25,7 @@
                                                                     <h1 style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#2f3133;font-size:19px;font-weight:bold;margin-top:0;text-align:left">Hello!</h1>
                                                                     <p style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left">
                                                                         Welcome to the online i-works Web-Portal.
-                                                                        {{\Auth::user()->name}} has added the comments<br>
+                                                                        {{\Auth::user()->name}} has added the comment<br>
                                                                         <h2 style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#2f3133;font-size:14px;font-weight:bold;margin-top:0;text-align:left">Comment</h2>
                                                                         {{$comment ?? ''}}
                                                                    </p>

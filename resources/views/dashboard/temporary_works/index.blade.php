327a780a13f6be1d1bc60f62103af4214d9dc7df
Hamza <Hamza.Alam@accrualgroup.com>
2022-04-27 14:35:37 +0500

    Code Change

--- resources/views/dashboard/temporary_works/index.blade.php
+++ resources/views/dashboard/temporary_works/index.blade.php
@@ -880,7 +880,7 @@ border-radius: 8px;
 
                                     </td>
                                     <td>
-                                        <p class="cursor-pointer" style="margin-bottom:0px;font-weight: 400;font-size: 14px;position: relative; top: -7px;" data-id="{{Crypt::encrypt($item->id)}}" data-desc="{{$item->design_requirement_text}}">Permit to<br> load</p>
+                                        <p class="cursor-pointer permit-to-load" style="margin-bottom:0px;font-weight: 400;font-size: 14px;position: relative; top: -7px;" data-id="{{Crypt::encrypt($item->id)}}" data-desc="{{$item->design_requirement_text}}">Permit to<br> load</p>
                                         @if(isset($item->permits[0]->id) || isset($item->scaffold[0]->id) )
                                          @php 
                                          $permitexpire=\App\Models\PermitLoad::where(['temporary_work_id'=>$item->id,'status'=>1])->whereDate('created_at', '<=',\Carbon\Carbon::now()->subDays(7))->count();

c4d6c50e29e0dc8b4cab550d7ec6224edda4a333
Abdul Basit <basitawan.abdul@gmail.com>
2022-05-06 11:18:52 +0500

    updated code

--- resources/views/dashboard/temporary_works/create.blade.php
+++ resources/views/dashboard/temporary_works/create.blade.php
@@ -806,8 +806,8 @@ height: 72px;
     
         });
         $("#scope-of-design #submit-requirment button").on("click", function() {
-            $("#scope-of-design").removeClass("blackBack")
-            $("#scope-of-design").addClass("whiteBack")
+            $("#scopofdesign").removeClass("blackBack")
+            $("#scopofdesign").addClass("whiteBack")
     
         });
         $("#attachment-of-design  #submit-requirment button").on("click", function() {

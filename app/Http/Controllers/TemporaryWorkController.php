59a01afda6e971271d597612f8869209b2ef37fd
obaidjani <muhammad.obaid@dazaran.com>
2022-05-31 12:53:46 +0500

    error resolved

--- app/Http/Controllers/TemporaryWorkController.php
+++ app/Http/Controllers/TemporaryWorkController.php
@@ -687,7 +687,8 @@ class TemporaryWorkController extends Controller
     public function load_scan_temporarywork(Request $request, $id)
     {
         $tempid =$request->temp;
-        $temporary_works = TemporaryWork::where(['project_id' => $id, 'tempid' => $tempid])->first();
+        $dtempid = Crypt::decryptString($request->temp);
+        $temporary_works = TemporaryWork::where(['project_id' => $id, 'tempid' => $dtempid])->first();
         $temporary_work_id=$temporary_works->id;
         $scantempwork = 'scantempwork';
         return view('dashboard.temporary_works.temporarywork-scan', compact('tempid','id','temporary_work_id','scantempwork'));

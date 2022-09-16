dfa96df7a36368096f1e84bbb5b17f0087317e22
Abdul Basit <basitawan.abdul@gmail.com>
2022-09-16 17:26:37 +0500

    updated code

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,6 +1,6 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100" style="background:transparent;">
     <div>
-       <a href="https://ctworks.co.uk/"><img alt="Logo" src="{{asset('assets/media/logos/logo2.png')}}"  style="max-width: 270px;" class="h-15px"/></a>
+       <a href="#"><img alt="Logo" src="{{asset('assets/media/logos/logo.png')}}"  style="max-width: 270px;" class="h-15px"/></a>
     </div>
 
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">

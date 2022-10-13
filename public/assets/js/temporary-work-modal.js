7a1a8e776543dcd70bba71d7a5cd72ab833ae466
Abdul Basit <basitawan.abdul@gmail.com>
2022-10-13 19:00:16 +0500

    updated text in modal

--- public/assets/js/temporary-work-modal.js
+++ public/assets/js/temporary-work-modal.js
@@ -52,7 +52,7 @@ $(document).ready(function () {
             key=key.replace(/\s/g, '');
             console.log(key);
             var list='';
-            list='<h3><input type="hidden" name="req_type" value="'+full_val+'"/>'+full_val+'</h3><table class="table"><tbody>';
+            list='<h3><input type="hidden" name="req_type" value="'+full_val+'"/>'+full_val+'</h3><p>Reminder of checklist suggested which you should provide for the designer to speed the process and results in accurate designs.</p><table class="table"><tbody>';
             for (let x in jsondata[key]) {
                list += '<tr><td><input type="hidden" name="req_name[]" value="'+x+'"/> '+x+'</td><td><input type="checkbox" name="req_check['+x+']" value="2"/></td><td><input type="text" name="req_notes[]" class="form-control"/></td></tr>';
             }

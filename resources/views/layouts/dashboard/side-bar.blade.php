1d9340755a79664114c287344f5761fe1d6ef856
obiafridi <obaid.afridi@accrualhub.com>
2022-11-03 15:13:10 +0500

    logo show

--- resources/views/layouts/dashboard/side-bar.blade.php
+++ resources/views/layouts/dashboard/side-bar.blade.php
@@ -3,7 +3,7 @@
     <div class="aside-logo flex-column-auto" id="kt_aside_logo">
         <!--begin::Logo-->
         <a href="{{url('dashboard')}}">
-            <img alt="Logo" src="{{asset('assets/media/logos/logo.png')}}" style="height:auto !important; width:100%; max-width:300px;" class="h-15px logo" />
+            <img alt="Logo" src="{{asset('assets/media/logos/logo.png')}}" style="height:auto !important; width:100%; max-width:300px;" class="h-15px logo"/>
         </a>
         <!--end::Logo-->
         <!--begin::Aside toggler-->

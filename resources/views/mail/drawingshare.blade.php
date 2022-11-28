12aa3f8165b0d8e0ac648a3b08cdafe72eaaef2e
Abdul Basit <basitawan.abdul@gmail.com>
2022-11-28 19:02:17 +0500

    mail content changes done

--- resources/views/mail/drawingshare.blade.php
+++ resources/views/mail/drawingshare.blade.php
@@ -24,7 +24,7 @@
                                                             <td style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;padding:35px"><span class="im">
                                                                     <h1>Hello!</h1>
                                                                     <p style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left">
-                                                                    <h3>Welcome to the online i-works Portal. 
+                                                                    <h3>Welcome to the online Temporary Works Portal. 
                                                                     </h3>
                                                                 
                                                                     <h5>

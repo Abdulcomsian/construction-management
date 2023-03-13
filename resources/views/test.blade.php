e19d22be46670ad5689ab6d4b7b41e3608540cd8
Abdul Basit <a.basit@accrualhub.com>
2023-03-13 19:03:30 +0500

    Css changes done

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -312,7 +312,12 @@
       color: #9D9D9D;
 
    }
-   
+   .document-nav{
+      padding: 12px;
+   }
+   .document-nav .navi-item{
+      list-style: none;
+   }
 </style>
 @include('layouts.sweetalert.sweetalert_css')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/dropzone.css" />
@@ -547,53 +552,13 @@
                         </div>
                         <div class="col-md-6">
                             <!--begin::Button-->
-                            <a href="{{ route('temporary_works.create') }}" class="btn pull-right btn-primary font-weight-bolder" style="color:white !important;border-radius:5px;">
+                            <a href="{{ route('temporary_works.create') }}" class="btn pull-right btn-primary font-weight-bolder" style="color:white !important;border-radius:5px; margin-left: 10px; border: 1px solid #07D564;">
                             <span class="fa fa-plus"></span> Design Brief</a>
                             <!--end::Button-->
+                           
                             <!--begin::Dropdown-->
-                            <div class="dropdown pull-right dropdown-inline mr-2 mt-0 mx-4" style="border-radius:5px;">
-                               <button type="button" class="btn  font-weight-bolder dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="border-radius:5px; overflow: hidden; color: #07D564; background-color: #07D5641A">
-                                  <!-- <span class="svg-icon svg-icon-md">
-                                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                           <rect x="0" y="0" width="24" height="24"></rect>
-                                           <path d="M3,16 L5,16 C5.55228475,16 6,15.5522847 6,15 C6,14.4477153 5.55228475,14 5,14 L3,14 L3,12 L5,12 C5.55228475,12 6,11.5522847 6,11 C6,10.4477153 5.55228475,10 5,10 L3,10 L3,8 L5,8 C5.55228475,8 6,7.55228475 6,7 C6,6.44771525 5.55228475,6 5,6 L3,6 L3,4 C3,3.44771525 3.44771525,3 4,3 L10,3 C10.5522847,3 11,3.44771525 11,4 L11,19 C11,19.5522847 10.5522847,20 10,20 L4,20 C3.44771525,20 3,19.5522847 3,19 L3,16 Z" fill="#000000" opacity="0.3"></path>
-                                           <path d="M16,3 L19,3 C20.1045695,3 21,3.8954305 21,5 L21,15.2485298 C21,15.7329761 20.8241635,16.200956 20.5051534,16.565539 L17.8762883,19.5699562 C17.6944473,19.7777745 17.378566,19.7988332 17.1707477,19.6169922 C17.1540423,19.602375 17.1383289,19.5866616 17.1237117,19.5699562 L14.4948466,16.565539 C14.1758365,16.200956 14,15.7329761 14,15.2485298 L14,5 C14,3.8954305 14.8954305,3 16,3 Z" fill="#000000"></path>
-                                        </g>
-                                     </svg>
-                                  </span> -->
-                                  Status
-                               </button>
-                               <!--begin::Dropdown Menu-->
-                               <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(-23px, -257px, 0px);" x-placement="top-end">
-                                  <!--begin::Navigation-->
-                                  <ul class="navi flex-column navi-hover py-2">
-                                    <li class="navi-item">
-                                        <a href="{{url('/temporary_works?status=all')}}" class="navi-link ">
-                       
-                                        <span class="navi-text">All</span>
-                                        </a>
-                                     </li>
-                                     <li class="navi-item">
-                                        <a href="{{url('/temporary_works?status=pending')}}" class="navi-link ">
-                       
-                                        <span class="navi-text">Pending</span>
-                                        </a>
-                                     </li>
-                                     <li class="navi-item">
-                                        <a href="{{url('/temporary_works?status=completed')}}" class="navi-link">
-                       
-                                        <span class="navi-text">Completed</span>
-                                        </a>
-                                     </li>
-                       
-                                  </ul>
-                               </div>
-                               <!--end::Dropdown Menu-->
-                            </div>
-                            <!--begin::Dropdown-->
-                            <div class="dropdown pull-right dropdown-inline mr-2 mt-0 " style="border-radius:5px;">
-                               <button type="button" class="btn  font-weight-bolder dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="border-radius:5px; overflow: hidden; color: #07D564; background-color: #00d4602b;">
+                            <div class="dropdown pull-right dropdown-inline mr-2 mt-0 " style="border-radius:5px; ">
+                               <button type="button" class="btn  font-weight-bolder dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="border-radius:5px; overflow: hidden; color: #07D564; background-color: #00d4602b; border: 1px solid #07D564;">
                                   <!-- <span class="svg-icon svg-icon-md">
                                      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
                                         <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -608,7 +573,7 @@
                                <!--begin::Dropdown Menu-->
                                <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(-23px, -257px, 0px);" x-placement="top-end">
                                   <!--begin::Navigation-->
-                                  <ul class="navi flex-column navi-hover py-2">
+                                  <ul class="navi flex-column navi-hover py-2 document-nav">
                                      <li class="navi-item">
                                         <a href="#" class="navi-link adddocument">
                                         <span class="navi-icon">
@@ -664,6 +629,47 @@
                                <!--end::Dropdown Menu-->
                             </div>
                             <!--end::Dropdown-->
+                             <!--begin::Dropdown-->
+                             <div class="dropdown pull-right dropdown-inline mr-2 mt-0 " style="border-radius:5px;">
+                               <button type="button" class="btn  font-weight-bolder dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="border-radius:5px; overflow: hidden; color: #07D564; background-color: #07D5641A; border: 1px solid #07D564; margin-right: 13px">
+                                  <!-- <span class="svg-icon svg-icon-md">
+                                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                           <rect x="0" y="0" width="24" height="24"></rect>
+                                           <path d="M3,16 L5,16 C5.55228475,16 6,15.5522847 6,15 C6,14.4477153 5.55228475,14 5,14 L3,14 L3,12 L5,12 C5.55228475,12 6,11.5522847 6,11 C6,10.4477153 5.55228475,10 5,10 L3,10 L3,8 L5,8 C5.55228475,8 6,7.55228475 6,7 C6,6.44771525 5.55228475,6 5,6 L3,6 L3,4 C3,3.44771525 3.44771525,3 4,3 L10,3 C10.5522847,3 11,3.44771525 11,4 L11,19 C11,19.5522847 10.5522847,20 10,20 L4,20 C3.44771525,20 3,19.5522847 3,19 L3,16 Z" fill="#000000" opacity="0.3"></path>
+                                           <path d="M16,3 L19,3 C20.1045695,3 21,3.8954305 21,5 L21,15.2485298 C21,15.7329761 20.8241635,16.200956 20.5051534,16.565539 L17.8762883,19.5699562 C17.6944473,19.7777745 17.378566,19.7988332 17.1707477,19.6169922 C17.1540423,19.602375 17.1383289,19.5866616 17.1237117,19.5699562 L14.4948466,16.565539 C14.1758365,16.200956 14,15.7329761 14,15.2485298 L14,5 C14,3.8954305 14.8954305,3 16,3 Z" fill="#000000"></path>
+                                        </g>
+                                     </svg>
+                                  </span> -->
+                                  Status
+                               </button>
+                               <!--begin::Dropdown Menu-->
+                               <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(-23px, -257px, 0px);" x-placement="top-end">
+                                  <!--begin::Navigation-->
+                                  <ul class="navi flex-column navi-hover py-2">
+                                    <li class="navi-item">
+                                        <a href="{{url('/temporary_works?status=all')}}" class="navi-link ">
+                       
+                                        <span class="navi-text">All</span>
+                                        </a>
+                                     </li>
+                                     <li class="navi-item">
+                                        <a href="{{url('/temporary_works?status=pending')}}" class="navi-link ">
+                       
+                                        <span class="navi-text">Pending</span>
+                                        </a>
+                                     </li>
+                                     <li class="navi-item">
+                                        <a href="{{url('/temporary_works?status=completed')}}" class="navi-link">
+                       
+                                        <span class="navi-text">Completed</span>
+                                        </a>
+                                     </li>
+                       
+                                  </ul>
+                               </div>
+                               <!--end::Dropdown Menu-->
+                            </div>
                             
                         </div>
                      </div>

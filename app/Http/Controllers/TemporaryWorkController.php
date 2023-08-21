3e02e0a6461744c6104470f5e5e1d4c0ee0a0f22
Haroon <haroonniaz96github@gmail.com>
2023-08-21 18:06:50 +0500

    new changes updated

--- app/Http/Controllers/TemporaryWorkController.php
+++ app/Http/Controllers/TemporaryWorkController.php
@@ -886,8 +886,8 @@ $notify_admins_msg = [
     {
         Validations::storeTemporaryWork($request);
         try {
-            if(!$request->approval || !$temporaryWork->status == 2){
-                $pdf_history = PdfFilesHistory::where('tempwork_id', $temporaryWork->id)->count();
+            if(!$request->approval){
+                $pdf_history = PdfFilesHistory::where([['tempwork_id', $temporaryWork->id],['type','design_brief']])->count();
                 if($pdf_history == 0){
                     HelperFunctions::PdfFilesHistory($temporaryWork->ped_url, $temporaryWork->id, 'design_brief', $temporaryWork->twc_id_no);
                 }
@@ -1033,7 +1033,7 @@ $notify_admins_msg = [
                 $model->ped_url = $filename;
                 $model->save();
                 if(!$request->approval || !$temporaryWork->status == 2){
-                $count = $model->designbrief_history->count();
+                $count = $model->pdfFilesDesignBrief->count();
                 $twc_id_no = $request->twc_id_no.'-'.$count++;
                 HelperFunctions::PdfFilesHistory($filename, $temporaryWork->id, 'design_brief', $twc_id_no);
                 }

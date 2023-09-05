9a09b4e1ba7ccaabc37d3635675f662321c75d64
Abdul Basit <basitawan.abdul@gmail.com>
2023-09-05 18:19:38 +0500

    added description

--- resources/views/dashboard/modals/description.blade.php
+++ resources/views/dashboard/modals/description.blade.php
@@ -28,7 +28,7 @@
                     <div class="row g-9 mb-8">
                         <!--begin::Col-->
                         <div class="col-md-12">
-                            <div class="col-lg-12 d-flex align-items-center fw-bold fs-6" id="desc">
+                            <div class="col-lg-12 align-items-center fw-bold fs-6" id="desc">
                             </div>
                             <!--begin::Label-->
                         </div>

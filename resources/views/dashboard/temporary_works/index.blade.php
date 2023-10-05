0127e8d2fcb4dab90a35da300f6d4db7c7d8bec6
Abdul Basit <basitawan.abdul@gmail.com>
2023-10-05 13:05:07 +0500

    udpated cdoe

--- resources/views/dashboard/temporary_works/index.blade.php
+++ resources/views/dashboard/temporary_works/index.blade.php
@@ -1255,12 +1255,7 @@ $tempWorkClass = "d-none";
                                                                     width="70px" height="70px">
                                                             </a>
                                                             @endif
-                                                            <!-- @if($item->pdfFilesDesignBrief->count() == 0) -->
-                                                                <a style="color:{{$item->status==0 || $item->status==2 ? 'red !important':'#07d564 !important';}}; "
-                                                                    target="_blank"
-                                                                    href="{{asset('pdf'.'/'.$item->ped_url)}}">{{$item->twc_id_no}}
-                                                                </a>
-                                                           <!--  @elseif($item->status==2 || $item->status == 0)  -->
+                                                          
                                                                 <a style="color:{{$item->status==0 || $item->status==2 ? 'red !important':'#07d564 !important';}}; "
                                                                     target="_blank"
                                                                     href="{{asset('pdf'.'/'.$item->ped_url)}}">{{$item->twc_id_no}}

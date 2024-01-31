75ca85bcc93b4fd5e633c3d21fac9ed0bc484335
MuneebAccrual <muneeb.rehman@accrualhub.com>
2024-01-31 12:29:37 +0500

    latest code push cron job issue resoloved

--- routes/web.php
+++ routes/web.php
@@ -389,29 +389,3 @@ Route::get("/test" , function(){
 
 
 Route::get('get-report' , [TemporaryWorkController::class , 'getReportsData'])->name('get-report');
-
-// Route::view('/testRoute','/layouts/pdf/signature');
-
-
-// Route::get('test', function(){
-//     $invoices = Invoice::where('status','Unpaid')->get();
-//     // dd($invoices);
-//         foreach($invoices as $invoice)
-//         {
-//             $invoice_date = date('Y-m-d',strtotime($invoice->created_at));
-//             $invoice_date = carbon::parse($invoice_date);
-//             $due_date = date('Y-m-d',strtotime($invoice->date_of_payment));
-//             $due_date = carbon::parse($due_date);
-//             $diff = $invoice_date->diffInDays($due_date);
-//             if($diff == 3 || $diff == 7 || $diff == 1)
-//             {
-//                 dispatch(new SendEmailJob($invoice->send_email,$invoice->file_name,$diff,$invoice->id));
-//                 echo "Email send to the day " . $diff . "<br>";
-//             }else{
-//                 echo "Days are not matching .Days are " . $diff . "<br>";
-//             }
-
-//         }
-// });
-
-// Route::view('/test', '/mail/invoiceDesignerMail');
